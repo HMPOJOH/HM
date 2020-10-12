930551e471d1a4e9555a70edbf4a84dfed37e7f7
Oscar Olsson <happybits@gmail.com>
2020-10-12 11:57:17 +0200

    -

--- CSharpCourse/CSharpCourse/Methods/Demo1.cs
+++ CSharpCourse/CSharpCourse/Methods/Demo1.cs
@@ -8,11 +8,33 @@ namespace CSharpCourse.Methods
     {
         public static void Run()
         {
-            Console.WriteLine("When do you start in the morning?");
+            Console.Write("When do you start in the morning? ");
             string startString = Console.ReadLine();
             var start = TimeSpan.Parse(startString);
 
-            Console.WriteLine($"Minutes: {start.Minutes}");
+            Console.Write("When do you go for lunch? ");
+            string lunchString = Console.ReadLine();
+            TimeSpan lunch = TimeSpan.Parse(lunchString);
+
+            Console.Write("When does your lunch end? ");
+            string lunchEndString = Console.ReadLine();
+            TimeSpan lunchEnd = TimeSpan.Parse(lunchEndString);
+
+            Console.Write("When do you go home from work? ");
+            string endString = Console.ReadLine();
+            TimeSpan end = TimeSpan.Parse(endString);
+
+            Console.WriteLine();
+            
+            TimeSpan worktime = lunch - start + end - lunchEnd;
+
+            // 6h 20min
+
+            // Hours: 6
+            // TotalHours: 6.33h
+
+            Console.WriteLine($"You have worked {worktime.Hours}h and {worktime.Minutes} minutes");
+            Console.WriteLine($"Total hours: {worktime.TotalHours}");
         }
     }
 }
