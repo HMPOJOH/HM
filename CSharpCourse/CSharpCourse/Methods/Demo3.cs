338ec60904a0f3a87dd0aa43236c006ca1ab2e57
Oscar Olsson <happybits@gmail.com>
2020-10-13 09:59:15 +0200

    -

---
+++ CSharpCourse/CSharpCourse/Methods/Demo3.cs
@@ -0,0 +1,46 @@
+﻿// Added validation so you can't enter to early times
+using System;
+
+namespace CSharpCourse.Methods
+{
+    public class Demo3
+    {
+        // until ---------> 9:40
+        public static void Run()
+        {
+            TimeSpan start = AskForTime("When do start to work in the morning?", TimeSpan.Zero);
+            TimeSpan lunch = AskForTime("When do you go for lunch?", start);
+            TimeSpan lunchEnd = AskForTime("When does your lunch end?", lunch);
+            TimeSpan end = AskForTime("When do you go home from work?", lunchEnd);
+
+            TimeSpan worktime = CalculateWorkTime(start, lunch, lunchEnd, end);
+
+            Console.WriteLine();
+            Console.WriteLine($"You have worked {worktime.Hours}h and {worktime.Minutes} minutes");
+
+        }
+
+        private static TimeSpan CalculateWorkTime(TimeSpan start, TimeSpan lunch, TimeSpan lunchEnd, TimeSpan end)
+        {
+            return lunch - start + end - lunchEnd;
+        }
+
+        private static TimeSpan AskForTime(string question, TimeSpan mustBeLaterThan)
+        {
+            while(true)
+            {
+                Console.Write(question + " ");
+                var answer = Console.ReadLine();
+
+                // (Alt-Enter for inline declaration) 
+                bool correctFormat = TimeSpan.TryParse(answer, out TimeSpan time);
+
+                if (correctFormat && time.Days == 0 &&  time > mustBeLaterThan)
+                {
+                    return time;
+                }
+            }
+        }
+    }
+}
+
--- CSharpCourse/CSharpCourse/Program.cs
+++ CSharpCourse/CSharpCourse/Program.cs
@@ -17,7 +17,7 @@ namespace CSharpCourse
             //Control.Demo2.Run();
             //Control.Demo3.Run();
 
-            Methods.Demo2.Run();
+            Methods.Demo3.Run();
 
 
             //Ctrl - F5 to run
