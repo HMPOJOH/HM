2499d8493233a3747b52514d02a83fad842a6bf4
Oscar Olsson <happybits@gmail.com>
2020-10-14 11:48:46 +0200

    -

--- CSharpCourse/MyProject.Core/Calculator.cs
+++ CSharpCourse/MyProject.Core/Calculator.cs
@@ -21,6 +21,8 @@ namespace MyProject.Core
             return _value;
         }
 
+        // todo: create Reset method (set _value to 0) + create tests for that method
+
         public void Subtract(string number)
         {
             if (int.TryParse(number, out int result))
